ef18947f5ebd98cb05ee76642ecd1796fffbf74d
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2018-07-13 21:55:37 -0400

    completion with styling

--- color.js
+++ color.js
@@ -1,3 +1,4 @@
+var numSquares = 6;
 var colors = generateRandomColor(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -9,17 +10,43 @@ var easyBtn = document.querySelector("#easybtn");
 var hardBtn = document.querySelector("#hardbtn");
 
 easyBtn.addEventListener("click",function(){
-this.classList.add("selected");
+
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+	numSquares = 3
+	colors = generateRandomColor(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+
+	for(var i=0; i < squares.length; i++){
+		if(colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		}else{
+			squares[i].style.display = "none";
+		}
+	}
 });
 
 hardBtn.addEventListener("click",function(){
-
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+	numSquares = 6;
+	colors = generateRandomColor(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+		squares[i].style.display = "block";
+	}
 });
 
 
 resetButton.addEventListener("click", function(){
+	this.textContent = "New Colors"
+
+	message.textContent = "";
 	//generate random color
-	colors = generateRandomColor(6);
+	colors = generateRandomColor(numSquares);
 	//pick random color
 	pickedColor = pickColor();
 	//change display for generated color
@@ -28,7 +55,7 @@ resetButton.addEventListener("click", function(){
 	for(var i=0; i<squares.length;i++){
 		squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 });
 
 colorDisplay.textContent = pickedColor;
@@ -43,6 +70,7 @@ for(var i=0;i<squares.length;i++){
 			resetButton.textContent = "Play Again?"
 			changeColors(clickedcolor);
 			h1.style.backgroundColor = clickedcolor;
+
 		}else{
 			this.style.backgroundColor = "#232323";
 			message.textContent = "Try Again"
